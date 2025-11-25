ec0f0473b5acddcddedfea782296ef629f95f3cf
Szymon Wysocki <364264@uwr.edu.pl>
2025-11-25 12:15:59 +0100

    Wykonanie podpunktu A w zadaniu 2

---
+++ 25_11_2025/zad2.py
@@ -0,0 +1,22 @@
+import turtle
+
+bok = 200
+predkosc = 1
+
+zolw = turtle.Pen()
+zolw.speed(predkosc)
+
+# Podpunkt A
+zolw.forward(bok)
+zolw.left(90)
+zolw.forward(bok)
+zolw.left(90)
+zolw.forward(bok)
+zolw.left(90)
+zolw.forward(bok * 2)
+zolw.right(90)
+zolw.forward(bok)
+zolw.right(90)
+zolw.forward(bok)
+zolw.right(90)
+zolw.forward(bok)
